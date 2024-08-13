818a0f1d26ff9a0affa9e47c5944f9013fad939d
MatheusMorandi <theu.morandi@gmail.com>
2024-08-13 17:12:40 +0000

    Conti

---
+++ app/src/main/java/application/model/aluno.java
@@ -0,0 +1,40 @@
+package application.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="alunos")
+public class aluno {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Column(nullable = false)
+    private String nome;
+
+    
+
+    
+}
